e97477d93e16f27ba06ddcc7600a985c8e38dbae
RickyJiang3625 <145377795+RickyJiang3625@users.noreply.github.com>
2024-10-21 10:22:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 let title = document.createElement("title");
 title.innerHTML = "Clicker";
 document.head.appendChild(title);
+
+let count = 0;
+function increment() {
+    count = count +1
+    console.log(count)
+    document.getElementById("count-el").innerHTML = count;
+    
+
+}
+function decrement() {
+    if(count > 0){
+    count = count - 1
+    }
+    console.log(count)
+    document.getElementById("count-el").innerHTML = count;
+    
+
+}
+function save(){
+    document.getElementById("saves").innerHTML +=count + "-"
+}
